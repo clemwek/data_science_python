6e8b16e80e30f68c4e7a6ea0c66ad04d29135d62
dhywty <clemwek@gmail.com>
2017-12-22 16:18:40 +0300

    tweepy sentiment analysis

--- pds_2/pyscript2.py
+++ pds_2/pyscript2.py
@@ -1,11 +1,21 @@
 import tweepy
-from textblob import testblob
+from textblob import TextBlob
 
 
-consumer_key = 
-consumer_secret = 
+consumer_key = "oBAU4g0oK3UmunK01O6WpmAVP"
+consumer_secret = "XRwdJr1Tskp1ifLtq6dOiR3uIsSocBRqWroO8IZICGsS0eqSyQ"
 
-access_token = 
-access_token_secret = 
+access_token = "278587203-dlQ0ZuLwJJa9ycqyve58vY5X7bpj56svtc6LLENi"
+access_token_secret = "8NvAoPVE0rTdWSr6o8DCdHsNmNodwNEYFj5RuVTJepBrM"
 
-auth = t
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('Trump')
+
+for tweet in public_tweets:
+    print(tweet.text)
+    analysis = TextBlob(tweet.text)
+    print(analysis.sentiment)
